a330a16d4449e472c7b06f5f74020dfe90704a6a
cheonjaehyun <sjok8701@gmail.com>
2022-06-27 12:57:03 +0900

    Feat: 스코프와 Provider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,12 +15,17 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    //private final MyLogger myLogger;
+    // 주입받는게 아니라 클래스를 찾음
+    private final ObjectProvider<MyLogger> myLoggersProvider;
+
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
+
         String requestURL = request.getRequestURI().toString();
+        MyLogger myLogger = myLoggersProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
